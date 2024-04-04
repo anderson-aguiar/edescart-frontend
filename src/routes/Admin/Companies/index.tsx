3e4cebe1b1d5016e2b045ef1523e1b12a203332b
Anderson Martins <anderson.aguiar@outlook.com.br>
2024-04-04 14:01:36 -0300

    Dynamic companies listing

--- src/routes/Admin/Companies/index.tsx
+++ src/routes/Admin/Companies/index.tsx
@@ -3,15 +3,36 @@ import editImg from '../../../assets/edit.svg';
 import deleteImg from '../../../assets/delete.svg';
 import addCompany from '../../../assets/addCompany.svg';
 import ButtonAdmin from '../../../components/ButtonAdmin';
-
+import * as companyService from '../../../services/company-service';
+import { useEffect, useState } from 'react';
+import { CompanyDTO } from '../../../models/company';
+type QueryParams = {
+    page: number;
+}
 export default function Companies() {
-    
+
+    const [isLastPage, setIsLastPage] = useState(false);
+
+    const [companies, setCompanies] = useState<CompanyDTO[]>([]);
+
+    const [queryParams, setQueryParams] = useState<QueryParams>({
+        page: 0
+    })
+    useEffect(() => {
+        companyService.findAll(queryParams.page)
+            .then(response => {
+                const nextPage = response.data.content;
+                setCompanies(companies.concat(nextPage));
+                setIsLastPage(response.data.last);
+            })
+    }, [queryParams])
+
     return (
         <>
             <section id="company-listing-section" className='ed-container'>
                 <div className='ed-mt20 ed-line-bottom ed-company-listing-itens'>
                     <h2 className=' ed-listing-section-title '>Pontos de Coleta</h2>
-                    <img src={addCompany} alt='Adicionar Ponto de Coleta' title='Add Novo'/>
+                    <img src={addCompany} alt='Adicionar Ponto de Coleta' title='Add Novo' />
                 </div>
                 <table className="ed-table ed-mt20 ed-mb20">
                     <thead>
@@ -21,16 +42,20 @@ export default function Companies() {
                         <th></th>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className='ed-tb576'>1</td>
-                            <td className='ed-txt-left'>Ponto de coleta</td>
-                            <td>
-                                <img className='ed-company-listing-btn' src={editImg} alt='Editar' />
-                            </td>
-                            <td>
-                                <img className='ed-company-listing-btn' src={deleteImg} alt='Deletar' />
-                            </td>
-                        </tr>
+                        {
+                            companies.map(company => (
+                                <tr key={company.id}>
+                                    <td className='ed-tb576'>{company.id}</td>
+                                    <td className='ed-txt-left'>{company.name}</td>
+                                    <td>
+                                        <img className='ed-company-listing-btn' src={editImg} alt='Editar' />
+                                    </td>
+                                    <td>
+                                        <img className='ed-company-listing-btn' src={deleteImg} alt='Deletar' />
+                                    </td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
 
                 </table>
